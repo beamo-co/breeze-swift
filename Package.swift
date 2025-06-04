3c6394125c3ba920eeb4f375513c7fa96feaf942
andreasss55 <andreas@breeze.cash>
2025-06-04 19:26:55 +0800

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "breeze-iap-swift",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "breeze-iap-swift",
+            targets: ["breeze-iap-swift"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "breeze-iap-swift"),
+        .testTarget(
+            name: "breeze-iap-swiftTests",
+            dependencies: ["breeze-iap-swift"]
+        ),
+    ]
+)
